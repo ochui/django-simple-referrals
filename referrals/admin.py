d5a53ba77ac83e6fb678e3f1bb8795510111274c
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-07-23 17:19:05 +0100

    remove Multileveladmin

--- referrals/admin.py
+++ referrals/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
-from .models import FlatReferral, Link, MultiLevelReferral
-
-
-@admin.register(MultiLevelReferral)
-class MultiLevelReferralAdmin(TreeAdmin):
-    form = movenodeform_factory(MultiLevelReferral)
-
+from .models import FlatReferral, Link
 
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
